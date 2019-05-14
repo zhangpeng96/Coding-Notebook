dcc3efdfcf3569cc3cb52019bcad299d35acc8a0
zhangpeng96 <sjiaomail@gmail.com>
2019-05-14 23:03:35 +0800

    数值改为浮点型、数据预先排序

--- code/python/20190514-将某时刻数据的增减转换成时间序列的合计变化/demo_test.py
+++ code/python/20190514-将某时刻数据的增减转换成时间序列的合计变化/demo_test.py
@@ -3,8 +3,10 @@ import json, time
 def whatever(data_dict, start_ts, end_ts, gap_ts):
     # 变量初始化
     i = 0
-    value_count = 0
-    result = []
+    value_count = 0.0
+    result = [{'ts': start_ts, 'value': 0.0}]
+    # 数据按时间戳增加排序
+    data_dict = sorted(data_dict, key=lambda e: e.__getitem__('ts'))
 
     for ts in range(start_ts, end_ts, gap_ts):
 
@@ -24,11 +26,11 @@ def whatever(data_dict, start_ts, end_ts, gap_ts):
 if __name__ == '__main__':
 
     data = [
-        {'ts': 1009, 'value': 5},
-        {'ts': 1022, 'value': 2},
-        {'ts': 1024, 'value': 15},
-        {'ts': 1029, 'value': 7},
-        {'ts': 1030, 'value': 9}
+        {'ts': 1009, 'value': 5.5},
+        {'ts': 1029, 'value': 7.6667},
+        {'ts': 1024, 'value': 15.75},
+        {'ts': 1022, 'value': 2.3333},
+        {'ts': 1030, 'value': 9.25}
     ]
 
     result = whatever(data, 1000, 1050, 3)
