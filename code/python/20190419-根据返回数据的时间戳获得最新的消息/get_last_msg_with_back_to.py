b3b0757d6dbc43f136778193c5d3693949e1786b
zhangpeng96 <sjiaomail@gmail.com>
2019-06-05 09:46:57 +0800

    缩进标准

--- code/python/20190419-根据返回数据的时间戳获得最新的消息/get_last_msg_with_back_to.py
+++ code/python/20190419-根据返回数据的时间戳获得最新的消息/get_last_msg_with_back_to.py
@@ -2,14 +2,14 @@ import time
 
 
 def getCurrentTime():
-	return int(time.time())
+    return int(time.time())
 
 
 def getLastMsg(response, back_to):
-    msg = []						# 返回值list
-    global last_order_time			# 调用全局变量
-    last_order_time -= back_to		# 时间回溯，回溯时长要看修正值、back_to以及该函数具体调用的时间
-    current_time = getCurrentTime()	# 当前时间
+    msg = []                        # 返回值list
+    global last_order_time          # 调用全局变量
+    last_order_time -= back_to      # 时间回溯，回溯时长要看修正值、back_to以及该函数具体调用的时间
+    current_time = getCurrentTime() # 当前时间
     # 对传入数据按时间戳降序排序，避免后续更新last_order_time时出错
     response = sorted(response, key = lambda e: e.__getitem__('timestamp'), reverse = True)
     # 遍历传入数据
@@ -26,13 +26,13 @@ def getLastMsg(response, back_to):
 
 
 if __name__ == '__main__':
-	# 数据初始化
-	response = [{"timestamp": 1559619529, "content": "最新消息"}]
-	# 
-	FIX_SECTION = 5						# 定义修正值，使初始的last_order_time小于current_time某一个值
-	last_order_time = getCurrentTime() - FIX_SECTION
-	# 
-	while (1):
-		msg = getLastMsg(response, 0)
-		print(msg)
-		time.sleep(60)
+    # 数据初始化
+    response = [{"timestamp": 1559619529, "content": "最新消息"}]
+    # 
+    FIX_SECTION = 5                     # 定义修正值，使初始的last_order_time小于current_time某一个值
+    last_order_time = getCurrentTime() - FIX_SECTION
+    # 
+    while (1):
+        msg = getLastMsg(response, 0)
+        print(msg)
+        time.sleep(60)
