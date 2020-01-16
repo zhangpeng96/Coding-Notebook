1cd855a140306ccc3e89d4f8d51ff84587da8a49
zhangpeng96 <sjiaomail@gmail.com>
2020-01-16 15:30:00 +0800

    整理文档

--- Code/Python/20190419-根据返回数据的时间戳获得最新的消息/get_last_msg_with_back_to.py
+++ Code/Python/20190419-根据返回数据的时间戳获得最新的消息/get_last_msg_with_back_to.py
@@ -1,9 +1,17 @@
-import time
+import time, json
 
 
 def getCurrentTime():
     return int(time.time())
 
+def readJSON(path):
+    with open(path) as f:
+        contents = f.read()
+    return json.loads(contents)
+
+def ts2String():
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(time.time())))
+    return time_str
 
 def getLastMsg(response, back_to):
     msg = []                        # 返回值list
@@ -19,7 +27,7 @@ def getLastMsg(response, back_to):
             res['timestamp'] == current_time
         # 判断order_time是否大于last_order_time，若大于则说明是最新消息，追加到msg中，并更新last_order_time
         if res['timestamp'] > last_order_time:
-            msg.append(res['content'])
+            msg.append(res['text'])
             last_order_time = res['timestamp']
     # 返回msg
     return msg
@@ -27,12 +35,20 @@ def getLastMsg(response, back_to):
 
 if __name__ == '__main__':
     # 数据初始化
-    response = [{"timestamp": 1559619529, "content": "最新消息"}]
+    # base_now = getCurrentTime()
+    # response = [
+    #     {"timestamp": base_now-10, "content": "最新消息"},
+    #     {"timestamp": base_now+10, "content": "最新消息2"},
+    #     {"timestamp": base_now+30, "content": "最新消息3"},
+    #     {"timestamp": 1559619529, "content": "历史消息4"}
+    # ]
     # 
     FIX_SECTION = 5                     # 定义修正值，使初始的last_order_time小于current_time某一个值
     last_order_time = getCurrentTime() - FIX_SECTION
     # 
     while (1):
+        response = readJSON('test.json')
         msg = getLastMsg(response, 0)
+        print(ts2String())
         print(msg)
-        time.sleep(60)
+        time.sleep(50)
